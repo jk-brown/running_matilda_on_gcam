a40ec147e3f9332943bf9fc0b293b67e28885b99
Joe Brown <joseph.brown.k@gmail.com>
2024-05-20 20:47:10 -0400

    Adding whole game function and testing

--- workflow/test_file.R
+++ workflow/test_file.R
@@ -1,7 +1,38 @@
-# 8 Test run on Hector with plot --------------------------------------------
+
+# 1 source functions ------------------------------------------------------
+
+source("workflow/utils.R")
+
+# 2 Using the functions piecewise and editing ini -----------------------------------------------------
+
+hector_emissions <- get_hector_emissions("workflow/data/gcam_emissions.dat")
+hector_luc_emissions <- get_luc_emissions("workflow/data/gcam_emissions.dat")
+full_hector_emissions <- rbind(hector_emissions, hector_luc_emissions)
+
+emissions_constraints <- get_emissions_constraints(full_hector_emissions)
+
+write.csv(emissions_constraints, "workflow/data/new_hector_emissions.csv", quote = FALSE, row.names = FALSE)
+
+write_emissions_constraint_file(hector_emissions_path = "workflow/data/new_hector_emissions.csv",
+                                old_emissions_constraint = "workflow/data/ssp119_emiss-constraints_rf.csv",
+                                new_name = "gcam_emissions",
+                                directory = "workflow/data")
+
+write_input_file(emissions_constraint_file = "workflow/data/gcam_emissions_constraints.csv",
+                 new_name = "gcam_emissions",
+                 directory = "workflow/data")
+
+
+# 3 using whole game function ---------------------------------------------
+
+write_emissions_input(gcam_data = "workflow/data/gcam_emissions.dat",
+                      old_emissions_constraint = "workflow/data/ssp119_emiss-constraints_rf.csv",
+                      directory = "workflow/data")
+
+# 4 Test run on Hector with plot --------------------------------------------
 
 # read in the new ini file and run hector with it 
-ini_file <- "workflow/data/new_emissions_constraint.ini" # update to whatever name of new file
+ini_file <- "workflow/data/gcam_emissions.ini" # update to whatever name of new file
 core <- hector::newcore(ini_file)
 hector::run(core, runtodate = 2100) #gcam_emissions.csv file only goes to 2005
 out <- fetchvars(core, 1750:2100)
